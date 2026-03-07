a15e42f4b232a40c7a1e7ae8b060a37583cbc5a1
William Simião <williamsimiao@gmail.com>
2026-03-07 17:15:12 -0300

    initial commit

---
+++ Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge
+//
+//  Created by William Simião on 07/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Challenge
+
+iOS project
+
+## Requirements
+
+- iOS 14.0+
+- Xcode 13.0+
+- Swift 5.0+
+
+## Setup
+
+1. Clone the repository
+2. Open the `.xcodeproj` or `.xcworkspace` file in Xcode
+3. Build and run
+
+## License
+
+MIT
